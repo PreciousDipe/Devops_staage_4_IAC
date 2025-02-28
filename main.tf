7fd7af2c99cf5509b6afa967d6b81f3da0444e08
preciousdipe <preciousdipe@gmail.com>
2025-02-28 23:23:41 +0100

    iac

---
+++ main.tf
@@ -0,0 +1,174 @@
+// Generate private key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH keypair to use in AWS"
+  default     = "hng.pem"
+}
+
+// Create a new key pair for connecting to the EC2 instance via ssh
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+// Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${var.key_name}"
+  }
+}
+
+// Create a security group
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_hng"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Create EC2 instance
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+
+  tags = {
+    Name = "hng server"
+  }
+
+  root_block_device {
+    volume_size = 0  # Size in GB
+  }
+
+  provisioner "file" {
+    source      = "deploy.yml"  # Path to your Ansible playbook
+    destination = "/home/ubuntu/deploy.yml"  # Where the playbook will be stored on the instance
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = tls_private_key.rsa_4096.private_key_pem
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo '[all]' > /home/ubuntu/inventory",
+      "echo 'localhost ansible_connection=local' >> /home/ubuntu/inventory",
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = tls_private_key.rsa_4096.private_key_pem
+    }
+  }
+
+  # Provisioner to install Ansible and run the playbook
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt update -y",
+      "sudo apt install -y ansible",
+      "ansible --version",  # Verify Ansible installation
+      "ansible-playbook -i /home/ubuntu/inventory /home/ubuntu/deploy.yml",
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = tls_private_key.rsa_4096.private_key_pem
+    }
+  }  
+
+}
+
+  
+// Outputs
+output "instance_ip" {
+  value = aws_instance.public_instance.public_ip
+}
+
+output "instance_user" {
+  value = "ubuntu"  # Assuming the AMI uses 'ubuntu' as the default user
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.rsa_4096.private_key_pem
+  sensitive = true
+}
